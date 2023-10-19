f8d0d8d77fcafe6a0c6023ce721be1b5b3dd0ec3
olenaslob <lena2805sl@gmail.com>
2023-10-18 21:59:36 -0600

    added

---
+++ scripts/w04-task.js
@@ -0,0 +1,80 @@
+/* LESSON 4 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Olena Slobodeniuk", // Replace with your name
+    photo: "images/placeholder.png", // Replace with the image's path and name
+    favoriteFoods: [
+      'Rice',
+      'Banana bread',
+      'Ravioli'
+    ],
+    hobbies: [
+      'Reading',
+      'Coding',
+      'Watching movies'
+    ],
+    placesLived: []
+};
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+  {
+    place: 'Rivne, Ukraine',
+    length: '41 years'
+  },
+  // Adding more places here
+  {
+    place: 'Thoiry, France',
+    length: '1 year'
+  },
+
+  {
+    place: 'Calgary, Canada',
+    length: '1 year'
+  },
+
+);
+
+/* DOM Manipulation - Output */
+
+/* Name */
+// Assign the value of the name property to the HTML element with ID 'name'
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Photo with attributes */
+// Assign the value of the photo property to the src attribute of the HTML <img> element
+document.querySelector('#photo').src = myProfile.photo;
+
+// Assign the value of the name property as the alt attribute of the HTML <img> element
+document.querySelector('#photo').alt = myProfile.name;
+
+/* Favorite Foods List*/
+// Create and append list items for favorite foods
+myProfile.favoriteFoods.forEach(food => {
+  let li = document.createElement('li');
+  li.textContent = food;
+  document.querySelector('#favorite-foods').appendChild(li);
+});
+
+/* Hobbies List */
+// Create and append list items for hobbies
+myProfile.hobbies.forEach(hobby => {
+  let li = document.createElement('li');
+  li.textContent = hobby;
+  document.querySelector('#hobbies').appendChild(li);
+});
+
+/* Places Lived DataList */
+// Create and append list items for places lived
+myProfile.placesLived.forEach(place => {
+  let dt = document.createElement('dt');
+  dt.textContent = place.place;
+  let dd = document.createElement('dd');
+  dd.textContent = place.length;
+  
+  let dl = document.querySelector('#places-lived');
+  dl.appendChild(dt);
+  dl.appendChild(dd);
+});
+
